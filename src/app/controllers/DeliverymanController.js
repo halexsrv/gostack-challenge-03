d7926b275eb9ae56656fa1ecc3ba7c9c3189cbf5
Alex Vitorino <halexsrv@gmail.com>
2020-02-11 20:12:20 -0300

    add deliveryman route

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,45 @@
+import Deliveryman from '../models/Deliveryman';
+// import File from '../models/File';
+
+class DeliverymanController {
+  async index(req, res) {
+    const deliverymen = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      // include: [
+      //   {
+      //     model: File,
+      //     as: 'avatar',
+      //     attributes: ['name', 'path', 'url'],
+      //   },
+      // ],
+    });
+
+    return res.json(deliverymen);
+  }
+
+  async store(req, res) {
+    const deliveryman = await Deliveryman.create(req.body);
+
+    return res.json(deliveryman);
+  }
+
+  async update(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+
+    const { id, name, email } = await deliveryman.update(req.body);
+
+    return res.json({ id, name, email });
+  }
+
+  async destroy(req, res) {
+    await Deliveryman.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.json();
+  }
+}
+
+export default new DeliverymanController();
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 
@@ -15,5 +20,12 @@ routes.get('/recipients', RecipientController.index);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.delete('/recipients/:id', RecipientController.destroy);
+
+routes.get('/deliverymen', DeliverymanController.index);
+routes.post('/deliverymen', DeliverymanController.store);
+routes.put('/deliverymen/:id', DeliverymanController.update);
+routes.delete('/deliverymen/:id', DeliverymanController.destroy);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

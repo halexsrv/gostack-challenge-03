d349615149fd8f2fffa840d09999b3a4457e3779
Alex Vitorino <halexsrv@gmail.com>
2020-02-13 19:51:38 -0300

    add notification data

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
@@ -28,6 +29,13 @@ class Database {
     // models
     //   .map(model => model.init(this.connection))
     //   .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
   }
 }
 
